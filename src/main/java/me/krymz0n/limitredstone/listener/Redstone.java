2e7b53a2bc250fce2ca45c72f02b5fb641362333
KRYMZ0N <jackgrimes05@yahoo.com>
2021-05-23 08:39:18 -0400

    Fixing more stuff

--- src/main/java/me/krymz0n/limitredstone/listener/Redstone.java
+++ src/main/java/me/krymz0n/limitredstone/listener/Redstone.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
 
 public class Redstone implements Listener {
     private final Main plugin;
@@ -25,6 +26,24 @@ public class Redstone implements Listener {
                     evt.setCancelled(true);
                     if (plugin.getConfig().getBoolean("Debug")) {
                         System.out.println("Prevented" + p.getName() + " from placing more redstone than allowed");
+                    }
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onChunkLoad(ChunkLoadEvent evt) {
+        Chunk c = evt.getChunk();
+        int cx = c.getX() << 4;
+        int cz = c.getZ() << 4;
+        for (int x = cx; x < cx + 16; x++) {
+            for (int z = cz; z < cz + 16; z++) {
+                for (int y = 0; y < 255; y++) {
+                    if (plugin.getConfig().getBoolean("LimitRedstoneOnChunkLoad") && !evt.isNewChunk() && c.getBlock(x, y, z).getType().equals(Material.REDSTONE_WIRE)) {
+                        if (plugin.checkChunk(Material.REDSTONE_WIRE, c) > plugin.getConfig().getInt("MaxRedstonePerChunk") && !(c.getBlock(x, y, z).getType() == null)) {
+                            c.getBlock(x, y, z).setType(Material.AIR);
+                        }
                     }
                 }
             }
